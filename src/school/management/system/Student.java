6c4c3572a88d2293de86fccb522a633e4abaad02
Ivan Tavarez <40846384+itava0@users.noreply.github.com>
2022-09-06 21:55:27 -0400

    New Student Class

---
+++ src/school/management/system/Student.java
@@ -0,0 +1,14 @@
+package school.management.system;
+
+
+/**
+
+ * Create by IGor on 09/06/2022
+ * This class is responsible for keeping
+ * track of students fees, name, grade & feed paid
+
+
+**/
+
+public class Student {
+}
