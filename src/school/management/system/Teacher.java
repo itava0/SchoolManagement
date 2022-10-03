8fac86db861b89dd859a92d671473b9c463f04d5
Ivan Tavarez <40846384+itava0@users.noreply.github.com>
2022-10-03 10:14:00 -0400

    teacher class

---
+++ src/school/management/system/Teacher.java
@@ -0,0 +1,26 @@
+package school.management.system;
+
+/**
+ * Created by itava on 10/03/2022
+ * This class is responsible for keeping the track
+ * of teacher's name, id, salary
+ */
+public class Teacher {
+
+    private int id;
+    private String name;
+    private  int salary;
+
+    /**
+     * Creates a new Teacher object.
+     * @param id id for the teacher.
+     * @param name name of the teacher.
+     * @param salary salary of the teacher.
+     */
+
+    public Teacher( int id, String name, int salary) {
+        this.id=id;
+        this.name=name;
+        this.salary=salary;
+    }
+}
